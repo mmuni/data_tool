e2ab3e6b5acd522755888888ac3468c748e3379d
Martin Atukunda <matlads@gmail.com>
2015-03-20 16:13:18 +0300

    Clean up the Users Controller

--- Controller/UsersController.php
+++ Controller/UsersController.php
@@ -1,61 +1,6 @@
 <?php
 App::uses('AppController', 'Controller');
 class UsersController extends AppController {
-
-
-
-	var $uses = array('Person','User');
-
-
-
-
-	
-    public $components = array('Paginator');
-
-
-	public function beforeFilter() {
-	parent::beforeFilter();
-          // Allow users to register and logout.
-               $this->Auth->allow('add', 'logout');
-              }
-	public function login() {
-
-		//if already logged-in, redirect
-		if($this->Session->check('Auth.User')){
-			$this->redirect(array('action' => 'dashboard'));
-		}
-
-		// if we get the post information, try to authenticate
-		if ($this->request->is('post')) {
-			if ($this->Auth->login()) {
-				$this->Session->setFlash(__('Welcome, '. $this->Auth->user('username')));
-				$this->redirect($this->Auth->redirectUrl());
-			} else {
-				$this->Session->setFlash(__('Invalid username or password'));
-			}
-
-		} 
-
-		}
-
-	
-	
-	
-
-
- public function add() {
-	 $this->set('people', $this->Person->find('list'));
-        if ($this->request->is('post')) {
-
-			$this->User->create();
-			if ($this->User->save($this->request->data)) {
-				$this->Session->setFlash(__('The user has been created'));
-				$this->redirect(array('action' => 'login'));
-			} else {
-				$this->Session->setFlash(__('The user could not be created. Please, try again.'));
-			}
-        }
-
     var $uses = array('User','Person','Role');
     public $components = array('Paginator');
 
@@ -69,10 +14,6 @@ class UsersController extends AppController {
     public function index() {
         $this->set('users', $this->Paginator->paginate());
     }
-
-
-	}
-
 
     public function dashboard(){
     }
@@ -108,7 +49,6 @@ class UsersController extends AppController {
             }
         }
     }
-
 
 }
 

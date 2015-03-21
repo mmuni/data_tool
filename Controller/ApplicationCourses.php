9853572b676548ab0a67709f16ee59bc663130d6
Baranga <barangaster@gmail.com>
2015-03-21 15:24:32 +0300

    created Application Course model and controller

---
+++ Controller/ApplicationCourses.php
@@ -0,0 +1,15 @@
+<?php
+class ApplicationCoursesController extends AppController {
+
+
+	public function add() {
+
+		}
+
+    public function beforeFilter(){
+        parent::beforeFilter();
+        $this->Auth->allow('add');
+    }
+
+
+}
